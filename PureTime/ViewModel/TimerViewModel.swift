7b7d9f2b5b938c1f0cfb4e68dfa3e81c301deb6c
Stephen Williams <107999+onato@users.noreply.github.com>
2024-06-03 14:59:54 +1200

    feat: Add view model with formatted elapsed time

---
+++ PureTime/ViewModel/TimerViewModel.swift
@@ -0,0 +1,18 @@
+import AVFoundation
+import Foundation
+import MediaPlayer
+
+class TimerViewModel: NSObject, ObservableObject {
+    @Published var elapsedTime = 0
+    
+    var formattedTime: String {
+        let hours = elapsedTime / 3600
+        let minutes = (elapsedTime / 60) % 60
+        let seconds = elapsedTime % 60
+        if hours > 0 {
+            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            return String(format: "%02d:%02d", minutes, seconds)
+        }
+    }
+}
