cc05251c64e115e365fd90919caf977790ac082b
Stephen Williams <107999+onato@users.noreply.github.com>
2024-06-03 15:07:54 +1200

    chore: Add the fancy meditation man

--- PureTime/ContentView.swift
+++ PureTime/ContentView.swift
@@ -7,6 +7,11 @@ struct ContentView: View {
         if viewModel.isPlaying {
             Text(viewModel.formattedTime)
         } else {
+            Image("Background")
+                .resizable()
+                .opacity(0.4)
+                .scaledToFit()
+                .frame(height: 180)
             Button(action: {
                 viewModel.togglePlay()
             }) {
