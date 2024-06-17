4a239eea01b573b25f9c6cffd52e840ed570dc00
Stephen Williams <107999+onato@users.noreply.github.com>
2024-06-17 12:13:44 +1200

    feat: Update to a smoother bell sound

--- PureTime/Sound/SoundManager.swift
+++ PureTime/Sound/SoundManager.swift
@@ -16,6 +16,7 @@ class SoundManager: NSObject, SoundManagerProtocol {
     weak var delegate: SoundManagerDelegate?
     
     lazy var player: AVAudioPlayer! = {
+        // Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=60767">Pixabay</a>
         let url = Bundle.main.url(forResource: "bell", withExtension: "mp3")!
         return try! AVAudioPlayer(contentsOf: url)
     }()
